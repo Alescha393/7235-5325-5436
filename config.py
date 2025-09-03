278db0f73b3f0ede36fa5f2fbc633a9c01bb95ef
Alescha393 <alekseishyokotov@yandex.ru>
2025-09-03 22:00:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Конфигурационные параметры бота
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Замените на токен вашего бота, полученный от @BotFather
+CHANNEL_ID = "YOUR_CHANNEL_ID_HERE"  # Замените на ID вашего канала (например, -1001234567890)
+SOURCE_CHAT_ID = "SOURCE_CHAT_ID_HERE"  # Замените на ID чата, из которого нужно пересылать сообщения
